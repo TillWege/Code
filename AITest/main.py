955eaf587586b1eef5c356975b86da3733162719
Tillwege <tillwege@gmail.com>
2022-10-10 19:35:24 +0200

    Updated AI-Test to use Conda

--- AITest/main.py
+++ AITest/main.py
@@ -8,48 +8,48 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 import PIL
-from tensorflow.keras import layers
+import keras.api._v2.keras as keras
 import time
 
 from IPython import display
 
 def make_generator_model():
     model = tf.keras.Sequential()
-    model.add(layers.Dense(7*7*256, use_bias=False, input_shape=(100,)))
-    model.add(layers.BatchNormalization())
-    model.add(layers.LeakyReLU())
+    model.add(keras.layers.Dense(7*7*256, use_bias=False, input_shape=(100,)))
+    model.add(keras.layers.BatchNormalization())
+    model.add(keras.layers.LeakyReLU())
 
-    model.add(layers.Reshape((7, 7, 256)))
+    model.add(keras.layers.Reshape((7, 7, 256)))
     assert model.output_shape == (None, 7, 7, 256)  # Note: None is the batch size
 
-    model.add(layers.Conv2DTranspose(128, (5, 5), strides=(1, 1), padding='same', use_bias=False))
+    model.add(keras.layers.Conv2DTranspose(128, (5, 5), strides=(1, 1), padding='same', use_bias=False))
     assert model.output_shape == (None, 7, 7, 128)
-    model.add(layers.BatchNormalization())
-    model.add(layers.LeakyReLU())
+    model.add(keras.layers.BatchNormalization())
+    model.add(keras.layers.LeakyReLU())
 
-    model.add(layers.Conv2DTranspose(64, (5, 5), strides=(2, 2), padding='same', use_bias=False))
+    model.add(keras.layers.Conv2DTranspose(64, (5, 5), strides=(2, 2), padding='same', use_bias=False))
     assert model.output_shape == (None, 14, 14, 64)
-    model.add(layers.BatchNormalization())
-    model.add(layers.LeakyReLU())
+    model.add(keras.layers.BatchNormalization())
+    model.add(keras.layers.LeakyReLU())
 
-    model.add(layers.Conv2DTranspose(1, (5, 5), strides=(2, 2), padding='same', use_bias=False, activation='tanh'))
+    model.add(keras.layers.Conv2DTranspose(1, (5, 5), strides=(2, 2), padding='same', use_bias=False, activation='tanh'))
     assert model.output_shape == (None, 28, 28, 1)
 
     return model
 
 def make_discriminator_model():
     model = tf.keras.Sequential()
-    model.add(layers.Conv2D(64, (5, 5), strides=(2, 2), padding='same',
+    model.add(keras.layers.Conv2D(64, (5, 5), strides=(2, 2), padding='same',
                                      input_shape=[28, 28, 1]))
-    model.add(layers.LeakyReLU())
-    model.add(layers.Dropout(0.3))
+    model.add(keras.layers.LeakyReLU())
+    model.add(keras.layers.Dropout(0.3))
 
-    model.add(layers.Conv2D(128, (5, 5), strides=(2, 2), padding='same'))
-    model.add(layers.LeakyReLU())
-    model.add(layers.Dropout(0.3))
+    model.add(keras.layers.Conv2D(128, (5, 5), strides=(2, 2), padding='same'))
+    model.add(keras.layers.LeakyReLU())
+    model.add(keras.layers.Dropout(0.3))
 
-    model.add(layers.Flatten())
-    model.add(layers.Dense(1))
+    model.add(keras.layers.Flatten())
+    model.add(keras.layers.Dense(1))
 
     return model
 
